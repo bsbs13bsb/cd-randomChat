2ee431453945bf5a1321acf04878139cc0a0397c
bsbs13bsb <105424641+bsbs13bsb@users.noreply.github.com>
2022-05-12 13:14:58 +0300

    botcally.py

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ https://t.me/BotFather يمكنك جلب التوكن الخاص بالبوت ع
 VERSION = "v2.2.1"
 
 # ضع توكن البوت هنا
-TOKEN = str("")
+TOKEN = str("5358874912:AAH-FwXogXSsEEIHCCR9tMQkUjMi1RcVz-s")
 
 # وضع الفرق بين كل رسالة ورسالة اخرى
 delay = 1 # 1s
 
 # اختيار عدد البلاغات الازمة لحظر العضو
-max_reports = 20 # 20
+max_reports = 10 # 10
 
 # اختار اقصى مدة للجلسة
 session_time = 60*20 # 20m
